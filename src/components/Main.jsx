67027f95b0350d20f873026e1787f68fccc00d8d
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-11 09:04:28 +0300

    Components defalcation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,10 @@ import ListBox from "./ListBox";
 import WatchBox from "./WatchBox";
 
 
-export default function Main() {
+export default function Main({children}) {
     return (
         <main className="main">
-            <ListBox />
-            <WatchBox />
+            {children}
         </main>
     )
 }
